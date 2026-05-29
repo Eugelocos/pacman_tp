120a8deedc35db310d7432ab89843120cc06e21a
Grauzilla <eugeniofalabella@gmail.com>
2026-05-29 17:08:24 -0300

    Se agregan nuevos sprites para los enemigos y se actualiza la lógica de carga de sprites en las clases de personajes

--- src/data_structures/character/character_data_type.py
+++ src/data_structures/character/character_data_type.py
@@ -14,7 +14,7 @@ class Character(Entity):
         "LEFT":(0,-1),   
         }
         self.nombre_ruta=sprite
-        self.ruta="pacman_tp/assets/images/"+self.nombre_ruta+"/"
+        self.ruta="pacman_tp/assets/images/"+self.nombre_ruta+"/"+self.nombre_ruta+"_1.png"
     def changeDir(self, new_dir):
         self.direction=new_dir
 
--- src/data_structures/character/enemy_data_type.py
+++ src/data_structures/character/enemy_data_type.py
@@ -9,6 +9,10 @@ class Enemy(Character):
 
     
     def change_sprite_by_direction(self):
-        self.ruta=f"pacman_tp/assets/images/{self.sprite}/"
+        self.ruta="pacman_tp/assets/images/"+self.nombre_ruta+"/"
         if self.direction[0]==0 and self.direction[1]==-1:
-            ruta=ruta+f"/abajo/{self.sprite}_abajo_.png"
+            self.ruta=self.ruta+f"/abajo/{self.sprite}_abajo_1.png"
+        elif self.direction[0]==0 and self.direction[1]==1:
+            self.ruta=self.ruta+f"/arriba/{self.sprite}_arriba_1.png"
+        elif self.direction[1]==0:
+            self.ruta=self.ruta+f"/lateral/{self.sprite}_horizontal_1.png"
