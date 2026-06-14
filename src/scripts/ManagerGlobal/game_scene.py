27ea49d0eef74d9c20e0e9de77c826454942150d
enievasvega-udesa <enievasvega@udesa.edu.ar>
2026-06-14 12:40:47 -0300

    arreglo bug commit

--- src/scripts/ManagerGlobal/game_scene.py
+++ src/scripts/ManagerGlobal/game_scene.py
@@ -35,8 +35,27 @@ class GameScene(EscenaBase):
         self.game_manager.score = 0
         if GameScene.muerte is None:
             GameScene.muerte=pygame.mixer.Sound(c.EFECTOS["vida_perdida"])  
+        if GameScene.comer_fantasma is None:
+            GameScene.comer_fantasma=pygame.mixer.Sound(c.EFECTOS["fantasma_comido"])  
+        if GameScene.sirena_normal is None:
+            GameScene.sirena_normal = pygame.mixer.Sound(c.EFECTOS["mov_fantasmas"])
+        if GameScene.sirena_power is None:
+            GameScene.sirena_power = pygame.mixer.Sound(c.EFECTOS["power_pellet"])
+        if GameScene.sirena_ojos is None:
+            GameScene.sirena_ojos = pygame.mixer.Sound(c.EFECTOS["ojos"])
+        if GameScene.waka_waka is None:
+            GameScene.waka_waka = pygame.mixer.Sound(c.EFECTOS["punto"])
+        
+        
+        # Guardamos el estado actual del audio para saber cuándo cambiarlo
+        self.estado_sirena_actual = None
+        
         self.power_up_timer=0
         self.power_up_duration=6000 #6 segs
+        self.fantasmas_comidos_racha = 0 
+        self.textos_puntajes = []
+        ruta_fuente = os.path.join("pacman_tp","assets", "fonts", "PressStart2P.ttf")
+        self.fuente_puntajes = pygame.font.Font(ruta_fuente, 8) #agregar fuente de retro
         
     def _jugador(self):
         """Busca y devuelve el jugador dentro de las entidades del juego.
@@ -100,6 +119,8 @@ class GameScene(EscenaBase):
 
                 for entidad in self.game_manager.entities:
                     if isinstance(entidad, Enemigo) and (entidad.state == "asustado" or entidad.state == "asustado_parpadeando"):
+                        entidad.state = "scatter"  #los fantasmas vuelven a su patrón de movimiento normal
+                        entidad.velocidad = c.VELOCIDAD_BASE*0.75
                         entidad.state = "scatter"
                         entidad.velocidad = c.VELOCIDAD_BASE*0.75
 
@@ -124,6 +145,13 @@ class GameScene(EscenaBase):
 
         for entity in self.game_manager.entities:
             entity.draw(self.game_manager.ventana)
+        
+        for texto in self.textos_puntajes:
+            # Renderizamos el texto (0, 255, 255) es un cyan/celeste brillante
+            superficie_texto = self.fuente_puntajes.render(texto[0], True, (0, 255, 255))
+            # Lo centramos en las coordenadas guardadas
+            rect_texto = superficie_texto.get_rect(center=(texto[1], texto[2]))
+            self.game_manager.ventana.blit(superficie_texto, rect_texto)
 
         pass
 
@@ -152,6 +180,8 @@ class GameScene(EscenaBase):
 
                                 entidad.state = "asustado"
                                 entidad.velocidad = c.VELOCIDAD_BASE * 0.5
+                                entidad.direction = (entidad.direction[0] * -1, entidad.direction[1] * -1) #invertir direccion
+                                entidad.proxima_direccion = entidad.direction
 
                                 centro_x, centro_y = entidad.rect.centerx, entidad.rect.centery
                                 centro_gxy = self.game_manager.grid_manager.world_to_grid((centro_x, centro_y))
